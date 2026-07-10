0ee5016feda032b81ca40760dabba25b4151cf2c
effortless <36704039+effort-less-g@users.noreply.github.com>
2026-07-10 21:46:59 +0400

    Update 1161. Maximum Level Sum of a Binary Tree.py

--- LeetCode 75/Binary Tree_BFS/1161. Maximum Level Sum of a Binary Tree.py
+++ LeetCode 75/Binary Tree_BFS/1161. Maximum Level Sum of a Binary Tree.py
@@ -1,1 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
 
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        
+        que = [(root, 1)]
+        counts = {}
+        ans, max_val = 1, root.val 
+        while que:
+
+            # print(max_val)
+            if que[0][0].left:
+                que.append((que[0][0].left, que[0][1]+1))
+            if que[0][0].right:
+                que.append((que[0][0].right, que[0][1]+1))
+            
+            if que[0][1] not in counts:
+                counts[que[0][1]] = que[0][0].val
+            else:
+
+                counts[que[0][1]] += que[0][0].val
+            
+            # print(counts[str(que[0][1])])
+
+            # print(que[0][0].val, que[0][1])
+        
+
+            que.pop(0)
+
+        for key in counts:
+            if  counts[key] > max_val:
+                ans = key
+                max_val = counts[key]
+
+        # print(counts)
+
+        return ans
+
+            
